268071f105d13ca4f9c967913d5d5a3e45ae2059
Marc <117731419+MarcHD3@users.noreply.github.com>
2024-04-25 11:50:45 +0200

    recreated file

---
+++ ApfelBaum/Marc/floatingNumbers.java
@@ -0,0 +1,19 @@
+package Marc;
+
+import java.util.Scanner;
+
+public class floatingNumbers {
+
+	    public static void main(String[] args) {
+	    	
+
+	        float first = 1.5f;
+	        float second = 2.0f;
+
+	        float gesamtZahl = first * second;
+
+	        System.out.println("The Number is: " + gesamtZahl);
+	        
+	        
+	    }
+	}
